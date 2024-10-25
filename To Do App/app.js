3df3447c296bac274aeb66eb491c1b8425c50c5b
Tushar Jetagi <112852721+TusharJetagi@users.noreply.github.com>
2024-10-25 23:17:39 +0530

    6th Project

---
+++ To Do App/app.js
@@ -0,0 +1,42 @@
+const inputBox = document.querySelector("#input-box");
+const listContainer = document.querySelector("#list-container");
+
+function AddTask() {
+  if (inputBox.value == "") {
+    alert("Please add Task");
+  } else {
+    let li = document.createElement("li");
+    li.style.color = "#002765";
+    li.style.fontSize = "18px";
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+listContainer.addEventListener(
+  "click",
+  (e) => {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showData();
